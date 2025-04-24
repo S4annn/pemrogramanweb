c74f93917de43812dfd131875b6ecfb35330a80d
unknown <sandyteukuprantaa15@gmail.com>
2025-04-24 16:36:51 +0700

    updatecode

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const toggleBtn = document.getElementById('darkModeToggle');
+  const icon = toggleBtn.querySelector('.icon');
+  const label = toggleBtn.querySelector('.label');
   const body = document.body;
 
+  // Cek preferensi tersimpan
   const savedTheme = localStorage.getItem('theme');
   if (savedTheme === 'dark') {
     body.classList.add('dark-mode');
-    toggleBtn.textContent = '☀️ Light Mode';
+    icon.textContent = '☀️';
+    label.textContent = 'Light Mode';
   }
 
   toggleBtn.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
-    
+
     if (body.classList.contains('dark-mode')) {
       localStorage.setItem('theme', 'dark');
-      toggleBtn.textContent = '☀️ Light Mode';
+      icon.textContent = '☀️';
+      label.textContent = 'Light Mode';
     } else {
       localStorage.setItem('theme', 'light');
-      toggleBtn.textContent = '🌙 Dark Mode';
+      icon.textContent = '🌙';
+      label.textContent = 'Dark Mode';
     }
   });
 
